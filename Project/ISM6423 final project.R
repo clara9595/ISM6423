6fcde01a9317f4f71932859f393f5058f63e898a
Aadit Kamat <aadit.kamat@ufl.edu>
2024-04-20 19:27:48 -0400

    Add graphs and remove code to join dataframes

--- Project/ISM6423 final project.R
+++ Project/ISM6423 final project.R
@@ -1,42 +1,20 @@
 library(dplyr)
+library(ggplot2)
 library(readr)
+library(maps)
 
-df_marketing <- read.csv("C:/Users/clare/Downloads/ISM 6423 final report datasets/Marketing_Data.csv")
-df_user <- read.csv("C:/Users/clare/Downloads/ISM 6423 final report datasets/User_Data.csv")
-df_advertising <- read.csv("C:/Users/clare/Downloads/ISM 6423 final report datasets/Advertising_Data.csv")
-
-# Inspecting data to find see if there is any missing value
-str(df_marketing)
-str(df_user)
-str(df_advertising)
-
-master_data <- right_join(df_user, df_advertising, by = "id")
+df_marketing <- read.csv("Marketing_Data.csv")
+df_user <- read.csv("User_Data.csv")
+df_advertising <- read.csv("Advertising_Data.csv")
 
 # Automatically convert all logical columns to integers
-clean_master <- master_data %>% mutate(across(where(is.logical), as.integer))
+df_user <- df_user %>% mutate(across(where(is.logical), as.integer)) %>% mutate()
 
-
-
-
-
-
-# Example of merging df_marketing and df_dummy on a common 'campaign_id'
-# First ensure that 'campaign_id' is of the same type in both dataframes
-df_marketing <- mutate(df_marketing, campaign_id = as.character(campaign_id))
-df_dummy <- mutate(df_dummy, campaign_id = as.character(campaign_id))
-
-# Merge marketing and dummy data on 'campaign_id'
-combined_df_1 <- left_join(df_marketing, df_dummy, by = "campaign_id")
-
-# Example of merging combined_df_1 with df_advertising on a common 'campaign_id'
-# Ensure that 'campaign_id' is of the same type in df_advertising
-df_advertising <- mutate(df_advertising, campaign_id = as.character(campaign_id))
-
-# Final merge to create a fully combined dataframe
-final_combined_df <- left_join(combined_df_1, df_advertising, by = "campaign_id")
-
-# View the structure of the final combined dataframe to verify it has merged correctly
-str(final_combined_df)
-
-# Save the final combined dataframe to a new CSV file, if needed
-write.csv(final_combined_df, "final_combined_data.csv", row.names = FALSE)
+# Generate box plot showing age range per platform by gender
+age_per_platform_by_gender <- ggplot(data=df_user, mapping=aes(x=platform, y=age)) + 
+  geom_boxplot() + 
+  facet_grid(gender ~ .)
+# Generate column charts showing interests per country based on demographics
+interests_per_country_by_demographics <- ggplot(data=df_user, mapping=aes(x=location, fill=interests)) + 
+  geom_bar(position=position_dodge(width = 0.8), binwidth = 25) + 
+  facet_grid(demographics ~ .)
